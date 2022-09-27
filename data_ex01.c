71ad09a31c71cc07981f77681c60a71ab0e31582
trace <1106029170@qq.com>
2022-09-27 18:06:25 +0800

    数据截断第二版

--- data_ex01.c
+++ data_ex01.c
@@ -5,7 +5,7 @@ const int N = 2000 + 10;
 int a[2010], n;
 
 
-void sort() {
+void sort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (a[i] > a[j]) {
@@ -25,12 +25,12 @@ double mean() {
     return sum /= n;
 }
 
-double middle() {
+double middle(int a[], int n) {
     // 0, 1, 2, 3
     if (n & 1) 
         return a[n/2] * 1.0;
     else 
-        return (a[n/2 - 1] + a[n/2]) * 1.0 / 2.0;
+        return (a[n/2] + a[n / 2 + 1]) * 1.0 / 2;
 }
 
 
@@ -71,24 +71,25 @@ double calMadMean() {
     return sum / n;
 }
 double calMadMiddle() {
-    double sum = 0.0, mid = middle();
+    double sum = 0.0, mid = middle(a, n);
+    int tt[2010];
     for (int i = 0; i < n; i++) {
-        sum += fabs(a[i] * 1.0 - mid);
+        tt[i] = fabs(a[i] - mid);
     }
-    
-    return sum / n;
+    sort(tt, n);
+    return middle(tt, n);
 }
 
 double calQuar() {
     
     int q1, q3;
-    int t = (n + 1) / 4;
-    if ((n + 1) % 4 == 0) {
+    int t = n / 4;  // 四分位点
+    if (n % 4 == 0) {  // 如果能被4整除
         q1 = a[t - 1];
         q3 = a[n - t];
         return (q3 - q1)*1.0;
-    } else {
-        return (a[n - t] + a[n - t -1] - a[t - 1] - a[t]) * 1.0 / 2;
+    } else {   // 如果不能被四整除 
+        return (a[n - t] + a[n - t - 1] - a[t - 1] - a[t]) * 1.0 / 2;
     }
 }
 
@@ -97,7 +98,7 @@ int main()
 {
     scanf("%d", &n);
     for (int i = 0; i < n ; i++) scanf("%d", &a[i]);
-    sort();
+    sort(a, n);
     //printf("%lf %lf %d %d\n", mean(), middle(), max(), min());
     printf("%.3lf %.3lf %.3lf %.3lf %.3lf\n", calRange(), calVar(), calMadMean(), calMadMiddle(), calQuar());
 
